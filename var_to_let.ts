930251b339a3ee777266eb51be194dbc8955dc1f
LiamrayWong <1079388320@qq.com>
2021-03-03 22:02:25 +0800

    feat: 将 var 转化成 let

--- var_to_let.ts
+++ var_to_let.ts
@@ -5,5 +5,18 @@ import generate from '@babel/generator';
 let code = 'var a = "let";var b = 2'
 
 const ast = parse(code,{sourceType:'module'})
-console.log(ast);
 
+// 遍历AST
+traverse(ast,{
+  // 每进入一个节点执行enter钩子函数
+  enter:item =>{
+    if(item.node.type === 'VariableDeclaration'){
+      if(item.node.kind === 'var'){
+        item.node.kind = 'let'
+      }
+    }
+  }
+})
+
+let result = generate(ast,{},code)
+console.log(result.code);
